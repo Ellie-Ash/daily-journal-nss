68d0ca187ff2a5490361e47900b15a009ebd9659
Ellie Ash <ellieash4000@gmail.com>
2019-04-12 11:50:57 -0500

    part 2 complete

--- scripts/journal.js
+++ scripts/journal.js
@@ -1,9 +1,50 @@
-const journalEntry = document.querySelectorAll("#content");
-console.log(journalEntry);
+/* foundation of the html */
+const myJournal = {
+    date: "a",
+    concepts: "b",
+    coffee: "c",
+    entry: "d",
+    mood: "e"
+}
+console.log("journal entry", myJournal);
 
-function entryInfo(evt){
-    console.log(evt);
+
+/* making blank array to push entries */
+var allEntries = [];
+allEntries.push(myJournal);
+
+console.log("entries", allEntries);
+
+/* practice */
+const myJournal2 = {
+    date: "dog",
+    concepts: "cat",
+    coffee: "horse",
+    entry: "fish",
+    mood: "mouse"
+}
+const myJournal3 = {
+    date: "chocolate",
+    concepts: "vanilla",
+    coffee: "blueberry",
+    entry: "strawberry",
+    mood: "orange"
 }
 
-const btnGetInfo = document.querySelector("#button");
-btnGetInfo.addEventListener("click", entryInfo);
+allEntries.push(myJournal2, myJournal3);
+console.log("test", allEntries);
+
+
+/* selecting all input fields */
+var journalEntry = document.querySelectorAll("#content");
+console.log(journalEntry);
+
+
+/* button */
+function newEntry(evt){
+    console.log(evt);
+    }
+    
+    const btnGetInfo = document.querySelector("#button");
+    btnGetInfo.addEventListener("click", newEntry);
+    
